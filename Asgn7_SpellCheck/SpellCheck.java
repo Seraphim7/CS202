5112ceca37222f7694d56bf8cb1240f3578d3c16
seraphim7 <seraphim7@outlook.com>
2020-03-05 10:40:19 -0800

    Functionality-wise, Asgn7 is done

--- Asgn7_SpellCheck/SpellCheck.java
+++ Asgn7_SpellCheck/SpellCheck.java
@@ -1,5 +1,6 @@
 
 import java.util.LinkedHashSet;
+import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -20,6 +21,8 @@ public class SpellCheck
 	{
 		setDictionary(dictionaryFileInput);
 		setText(textFileInput);
+		
+		printMisspelledWordsAlphabetically();
 	}
 
 	public void setDictionary(Scanner dictionaryFileInput)
@@ -53,26 +56,29 @@ public class SpellCheck
 		{
 			textFileline = textFileInput.nextLine();
 			
-			textParsedFileline = Utility.splitByDelimeter(textFileline, " ");
-			
-			for (String token : textParsedFileline)
+			if (!textFileline.isEmpty())
 			{
-				token = token.toLowerCase();
-				token = invalidCharacterStripper(token);
+				textParsedFileline = Utility.splitByDelimeter(textFileline, " ");
 				
-				if (checkTextWordAgainstDictionary(token))
+				for (String token : textParsedFileline)
 				{
-					textToSpellCheck.add(token);
-				}
-				else
-				{	
-					if (misspelledWordCountContainer.containsKey(token))
-					{
-						i = misspelledWordCountContainer.get(token);
-						misspelledWordCountContainer.remove(token);
-					}
+					token = token.toLowerCase();
+					token = invalidCharacterStripper(token);
 					
-					misspelledWordCountContainer.put(token, i + 1);
+					if (checkTextWordAgainstDictionary(token))
+					{
+						textToSpellCheck.add(token);
+					}
+					else
+					{	
+						if (misspelledWordCountContainer.containsKey(token))
+						{
+							i = misspelledWordCountContainer.get(token);
+							misspelledWordCountContainer.remove(token);
+						}
+						
+						misspelledWordCountContainer.put(token, i + 1);
+					}
 				}
 			}
 		}
@@ -81,6 +87,9 @@ public class SpellCheck
 	public boolean checkTextWordAgainstDictionary(String textToken)
 	{
 		textToken = textToken.toLowerCase();
+		
+		textToken = textToken.stripLeading();
+		textToken = textToken.stripTrailing();
 		
 		return dictionary.contains(textToken);
 	}
@@ -93,12 +102,36 @@ public class SpellCheck
 		{
 			charAtTokenIndex = token.charAt(i);
 			
-			if (charAtTokenIndex < 'A' || charAtTokenIndex > 'z')
+			if (charAtTokenIndex < 'A' || charAtTokenIndex > 'z' || (charAtTokenIndex > 'Z' && charAtTokenIndex < 'a'))
 			{
 				token = token.replace(charAtTokenIndex, ' ');
+			}
+			
+			if (i == token.length() - 1 && (charAtTokenIndex == ',' || charAtTokenIndex == '!' || charAtTokenIndex == '?' || charAtTokenIndex == '.'))
+			{
+				token = token.substring(0, token.length() - 1);
 			}
 		}
 		
 		return token;
+	}
+	
+	public void printMisspelledWordsAlphabetically()
+	{
+		String entryKey;
+		Integer entryValue;
+		
+		System.out.println();
+		
+		for (Entry<String, Integer> misspelledWord : misspelledWordCountContainer.entrySet())
+		{
+			entryKey = misspelledWord.getKey();
+			entryValue = misspelledWord.getValue();
+			
+			System.out.print(entryKey);
+			System.out.print(" (");
+			System.out.print(entryValue);
+			System.out.println(")");
+		}
 	}
 }
